0fcc9c58ace7d6cbca3f7c5ad45fe4a65b083c3c
tanishq <tanishq2326@gmail.com>
2026-08-24 15:23:00 +0530

    added hellojenkins.java file

---
+++ HelloJenkins.java
@@ -0,0 +1,8 @@
+public class HelloJenkins {
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println("      Hello from Jenkins!");
+        System.out.println("      Build was successful.");
+        System.out.println("=================================");
+    }
+}
